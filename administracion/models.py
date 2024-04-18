0088e6ace42a1a1c24c13dcabd6cacf7a0879dc4
Umbrark <diegu56@gmail.com>
2024-04-17 23:27:57 -0600

    Agregado del front en Registro de convocatoria por parte del alumno

--- administracion/models.py
+++ administracion/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# Formulario del documento a subir
+
+# class DocumentacionAdmin (models.Model):
+#    nombre_del_documento = models.CharField(verbose_name = "Nombre del documento", max_length=75, blank=False, null=False)
+#    resumen = models.TextField(verbose_name="Resumen", max_length=200, blank=False, null=False)
+#    nombre_del_autor = models.CharField(verbose_name="Nombre del autor", max_length=30, blank=False, null=False)
+#    sinodales = models.CharField(verbose_name="Sinodales", max_length=100, blank=False, null=False)
+#    fecha_de_subida = models.DateField(verbose_name="Fecha", blank=False, null=False)
+#    convocatoria_de_titulacion = models.TextField(verbose_name="Convocatria de titulacion", max_length=100, blank=False, null=False)
+    
+    
